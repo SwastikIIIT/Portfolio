d4fc7f77c715df176f2b68839138774ef3dbf5e1
SwastikIIIT <whirphool123@gmail.com>
2025-08-15 17:31:10 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,4 @@ npm dlx shadcn@latest add button
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
 })
